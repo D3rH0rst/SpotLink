2e9948dc07a257edee249cc1901f46f94ec7f4c2
Maximilian Schlepphorst <maximilian.schlepphorst@3m5.de>
2025-08-12 15:46:21 +0200

    another fix to submodule loading and propertysheets

--- SpotLinkVS/fetchMinhook.bat
+++ SpotLinkVS/fetchMinhook.bat
@@ -9,7 +9,7 @@ set "MINHOOK_DIR=%SOL_DIR%external\minhook"
 set "MINHOOK_LIB_DIR=%MINHOOK_DIR%\build\VC17\lib\%CONFIG%"
 
 REM Step 1: Clone if missing
-if not exist "%MINHOOK_DIR%" (
+if not exist "%MINHOOK_DIR%\.git" (
     echo Cloning MinHook from GitHub...
     git clone https://github.com/TsudaKageyu/minhook.git "%MINHOOK_DIR%" --depth=1
 )
